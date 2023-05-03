164ee4b0dbff11b59a78ab8d706bd1d2a4e31320
문지은 <mjieun0956@gmail.com>
2023-05-03 17:38:42 +0900

    Vue practice

---
+++ Vue/04. Vue component/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border">
+    <h1>This is my Component</h1>
+    <MyChild/>
+    <MyChild/>
+    <MyChild/>
+  </div>
+</template>
+
+<script>
+// 자식 컴포넌트
+import MyChild from '@/components/MyChild.vue'
+
+export default {
+    name: 'MyComponent',
+    components: {
+        MyChild,
+    }
+
+}
+</script>
+
+<style>
+.border { 
+    border: solid;
+}
+</style>
