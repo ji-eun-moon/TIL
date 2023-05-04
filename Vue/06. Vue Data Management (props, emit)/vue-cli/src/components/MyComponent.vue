b12f15eb90a9c20ccce7dfef37678f40169e13f8
문지은 <mjieun0956@gmail.com>
2023-05-04 10:42:52 +0900

    Vue practice

---
+++ Vue/06. Vue Data Management (props, emit)/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild 
+    static-props="component에서 child로" 
+    :my-props="dynamicProps"
+    @child-to-parent="parentGetEvent"
+    @child-input="getDynamicData"
+    />
+    <!-- <MyChild 
+    static-props="1" 
+    :my-props="1"
+    /> -->
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    MyChild,
+  },
+  data : function (){
+    return {
+      dynamicProps: "It's in data"
+    }
+  },
+  methods: {
+    parentGetEvent: function(inputData){
+      console.log("자식 컴포넌트에서 발생한 이벤트!")
+      console.log(`child에서 보낸 ${inputData} 보냄`)
+    },
+    getDynamicData(input) {
+      console.log(`child로부터 받은 ${input}을 출력`)
+    }
+  }
+}
+
+</script>
+
+<style>
+  .border {
+    border: solid;
+  }
+</style>
