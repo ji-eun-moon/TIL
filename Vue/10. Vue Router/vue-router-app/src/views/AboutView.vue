21fa5d6fb74d83cb07b4eeda9b494cc1bc278003
문지은 <mjieun0956@gmail.com>
2023-05-09 17:52:29 +0900

    Vue practice

---
+++ Vue/10. Vue Router/vue-router-app/src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{ name: 'home'}">Home</router-link> |
+    <!-- 프로그래밍 방식 네비게이션 -->
+    <button @click="toHome">홈으로!!!</button>
+    <br>
+    <!-- AboutView에서 데이터를 입력 받아 HelloView로 이동 -->
+    <input type="text" @keyup.enter="goToHello" v-model="inputData">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      inputData: null,
+    }
+  },
+  methods: {
+    toHome() {
+      this.$router.push({name: 'home'})
+    },
+    goToHello() {
+      this.$router.push({name: 'hello', params: { userName: this.inputData}})
+    }
+  }
+}
+</script>
---
+++ Vue/10. Vue Router/vue-router-app/src/views/HelloView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- <h1>hello, {{ $route.params.userName }}</h1> -->
+    <h1>hello, {{ userName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HellowView',
+    data() {
+        return {
+            userName: this.$route.params.userName
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
