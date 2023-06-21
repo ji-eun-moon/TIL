cf81d2a078bc9fd69663e5bed82e4541a3db1066
문지은 <mjieun0956@gmail.com>
2023-06-22 01:13:33 +0900

    Java Practice

---
+++ Java/Chapter1/src/ch05/IntVariableTest.java
@@ -0,0 +1,14 @@
+package ch05;
+
+public class IntVariableTest {
+
+	public static void main(String[] args) {
+		byte bs = 127;
+		System.out.println(bs);
+		
+		// int iVal = 12345678900;  // 오류
+		long iVal = 12345678900L;
+		System.out.println(iVal);
+	}
+
+}
