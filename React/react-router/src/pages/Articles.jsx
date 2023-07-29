91ec6c9978b211d29cc9042eabac86fea22dd239
문지은 <mjieun0956@gmail.com>
2023-07-29 21:47:59 +0900

    React Practice - React Router

---
+++ React/react-router/src/pages/Articles.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom';
+
+
+const activeStyle = {
+    color: 'green',
+    fontSize: 21,
+  };
+
+function Articles() {
+  return (
+    <div>
+      <Outlet />
+      <ul>
+        <li>
+          <NavLink
+            to="/articles/1"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/articles/2"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 2
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/articles/3"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 3
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Articles
