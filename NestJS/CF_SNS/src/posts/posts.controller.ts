02c892b0f49093115d5aa3a37770f71f41a4a8e7
문지은 <mjieun0956@gmail.com>
2024-04-09 01:56:17 +0900

    [NestJS] Controller, Service

--- NestJS/CF_SNS/src/posts/posts.controller.ts
+++ NestJS/CF_SNS/src/posts/posts.controller.ts
@@ -1,26 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts - 모든 posts를 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000000,
-      commentCount: 99999999,
-    };
+  getPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  // 2) GET /posts/:id - id가 1인 post를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  // 3) POST /posts - post를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  // 4) PATCH /posts/:id - id에 해당하는 POST를 변경한다.
+  @Patch(':id')
+  patchPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
+
+  // 5) DELETE /posts/:id - id에 해당하는 POST를 삭제한다.
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
   }
 }
--- NestJS/CF_SNS/src/posts/posts.service.ts
+++ NestJS/CF_SNS/src/posts/posts.service.ts
@@ -1,4 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000000,
+    commentCount: 99999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 혜린',
+    content: '노래연습하고 있는 혜린',
+    likeCount: 100000000,
+    commentCount: 99999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연중인 로제',
+    likeCount: 100000000,
+    commentCount: 99999999,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    // find 함수 : 존재하지 않을 경우 undefined 반환
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(id: number, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
+
+  deletePost(id: number) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    posts = posts.filter((post) => post.id !== +id);
+    return id;
+  }
+}
