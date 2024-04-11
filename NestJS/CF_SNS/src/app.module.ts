24ded431e5aa8c279edb83a833e88f8f6d8664ea
문지은 <mjieun0956@gmail.com>
2024-04-11 12:12:51 +0900

    chore: docker compose file init

--- NestJS/CF_SNS/src/app.module.ts
+++ NestJS/CF_SNS/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // TypeOrm 과 Database sync 자동으로 맞춤 - 개발환경에서만 사용
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
